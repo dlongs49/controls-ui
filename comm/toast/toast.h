3f3311805baa6fd46b34cc8dd008a80659b2f52a
dinglong <dillonl49@163.com>
2024-03-11 17:30:37 +0800

    style:#取消无用的控件引用

--- comm/toast/toast.h
+++ comm/toast/toast.h
@@ -2,23 +2,13 @@
 #ifndef CONTROLS_UI_TOAST_H
 #define CONTROLS_UI_TOAST_H
 
-#include <QMainWindow>
-#include <QWidget>
-#include <QMainWindow>
 #include <QHBoxLayout>
-#include <QVBoxLayout>
-#include <QLineEdit>
-#include <QPushButton>
-#include <QMessageBox>
-#include <QProgressBar>
 #include <QTimer>
-#include <QFileInfo>
 #include <QLabel>
-#include <QPropertyAnimation>
 #include <QDebug>
-#include <QFrame>
-#include <QGraphicsDropShadowEffect>
 #include <QPixmap>
+#include <QPropertyAnimation>
+#include <QGraphicsDropShadowEffect>
 class Toast : public QFrame {
 Q_OBJECT
 public:
@@ -35,14 +25,11 @@ public slots:
     void error(int main_width,QString str);
     void onMsgTimeOut();
 private:
-    QPushButton *btn;
     QSize parentSize;
     QHBoxLayout *layout;
-    QWidget *widget;
     QTimer *timer;
     QLabel *label;
     QLabel *img_label;
-    QFrame *frame;
     QPixmap *pixmap;
     QGraphicsDropShadowEffect *shadow;
     QPropertyAnimation *animation;
